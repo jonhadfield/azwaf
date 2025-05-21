66fdc9241e117fc2561d07a2f5587d3f5a4ebf01
Jon Hadfield <jon@lessknown.co.uk>
2025-05-21 23:15:28 +0100

    Add cache tests and simplify functions

---
+++ policy/tojson_test.go
@@ -0,0 +1,22 @@
+package policy
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestToJSONBasic(t *testing.T) {
+	s, err := toJSON("{\"a\":1}")
+	require.NoError(t, err)
+	require.Equal(t, "{\"a\":1}", s)
+
+	b, _ := json.Marshal("hello")
+	s, err = toJSON(b)
+	require.NoError(t, err)
+	require.Equal(t, "hello", s)
+
+	_, err = toJSON(123)
+	require.Error(t, err)
+}
