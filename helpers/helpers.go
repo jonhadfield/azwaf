6b2935ec74283f61f2f7f1af00581708695be0eb
Jon Hadfield <jon@lessknown.co.uk>
2025-11-20 11:47:16 +0000

    fix GetFunctionName to show the correct one.

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -14,7 +14,14 @@ func callerName(skip int) string {
 }
 
 // GetFunctionName returns the name of the calling function.
-func GetFunctionName() string { return callerName(1) }
+func GetFunctionName() string {
+	pc, _, _, _ := runtime.Caller(1) //nolint:dogsled
+
+	complete := runtime.FuncForPC(pc).Name()
+	split := strings.Split(complete, "/")
+
+	return split[len(split)-1]
+}
 
 // GetParentFunctionName returns the name of the parent of the calling function.
 func GetParentFunctionName() string { return callerName(3) }
